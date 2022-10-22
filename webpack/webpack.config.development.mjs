8ad7fcfe425599513ccecc34e40bce9d9ee82d25
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-10-22 18:55:50 +0530

    chore: remove all

--- webpack/webpack.config.development.mjs
+++
@@ -1,9 +0,0 @@
-export default () => ({
-	mode: "development",
-	devServer:{
-		port:3000,
-		open:true,
-		historyApiFallback:true,
-		watchFiles:["./web/**/**/**/**/*.{js,jsx,ts,tsx,css}"]
-	}
-});
